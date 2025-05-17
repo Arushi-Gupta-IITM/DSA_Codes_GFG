9d8090abe963be8e8761a2295a5332db87511a0e
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2025-05-17 12:09:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count pair sum/count-pair-sum.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            int sum = Integer.parseInt(read.readLine());
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            line = read.readLine();
+            tokens = line.split(" ");
+            for (String token : tokens) {
+                array2.add(Integer.parseInt(token));
+            }
+
+            int[] arr1 = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr1[idx++] = i;
+
+            int[] arr2 = new int[array2.size()];
+            idx = 0;
+            for (int i : array2) arr2[idx++] = i;
+
+            int res = new Solution().countPairs(arr1, arr2, sum);
+
+            System.out.println(res);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    // function to count all pairs
+    // from both the sorted arrays
+    // whose sum is equal to a given
+    // value
+    static int countPairs(int arr1[], int arr2[], int x) {
+        // code here.
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        
+        int i = 0;
+        int j = n2-1;
+        
+        int count = 0;
+        
+        while(i < n1 && j >= 0) {
+            int sum = arr1[i] + arr2[j];
+            
+            if(sum == x) {
+                count++;
+                i++;
+                j--;
+            } else if(sum > x) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        
+        return count;
+    }
+}
