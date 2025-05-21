ee5c4144fafaa74b5b58cc5b71c3d6ac510b2e64
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2025-05-21 06:08:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print 1 to n without using loops/print-1-to-n-without-using-loops.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    static void printTillN(int N) {
+        // code here
+        if(N == 1) {
+            System.out.print("1 ");
+            return;
+        }
+        
+        printTillN(N-1);
+        System.out.print(N+" ");
+    }
+}
