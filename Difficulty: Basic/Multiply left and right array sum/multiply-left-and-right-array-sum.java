2065e797ce58b81a860bdf920a3dfa27f743bea8
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2025-05-22 15:18:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiply left and right array sum/multiply-left-and-right-array-sum.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    public int multiply(int[] arr) {
+        // code here
+        int n = arr.length;
+        int mid = n/2;
+        
+        int leftSum = 0;
+        int rightSum = 0;
+        
+        for(int i=0; i<mid; i++) {
+            leftSum += arr[i];
+        }
+        
+        for(int i=mid; i<n; i++) {
+            rightSum += arr[i];
+        }
+        
+        return leftSum * rightSum;
+    }
+}
