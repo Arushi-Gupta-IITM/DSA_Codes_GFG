408af1d26c3bc87735ab102ce148f53585b1373e
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2025-05-23 10:05:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Power Using Recursion/power-using-recursion.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    static int RecursivePower(int n, int p) {
+        // add your code here
+        if(p == 0) return 1;
+        return n * RecursivePower(n, p-1);
+    }
+}
